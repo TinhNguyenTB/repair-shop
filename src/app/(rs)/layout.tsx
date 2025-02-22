12b145c42c9b479271716fe79194af120f1b79b5
TinhNguyenTB <tinh271103@gmail.com>
2025-02-22 15:21:25 +0700

    Routing Groups, Layouts & Templates

---
+++ src/app/(rs)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+export default function RSLayout({ children }: { children: ReactNode }) {
+    return (
+        <div className="mx-auto w-full max-w-7xl">
+            {/* Header */}
+            <div className="px-4 py-2">
+                {children}
+            </div>
+        </div>
+    )
+}
