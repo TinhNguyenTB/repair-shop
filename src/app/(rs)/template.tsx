8e84d7d5955419d0d14c20be881be40f195b72cc
TinhNguyenTB <tinh271103@gmail.com>
2025-02-22 16:18:53 +0700

    Custom Animations with Tailwind

--- src/app/(rs)/template.tsx
+++ src/app/(rs)/template.tsx
@@ -1,6 +1,6 @@
 export default function Template({ children }: { children: React.ReactNode }) {
     return (
-        <div>
+        <div className="animate-appear">
             {children}
         </div>
     )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { NavButton } from "@/components/NavButton";
 
 export function Header() {
     return (
-        <header className="bg-background h-12 p-2 border-b sticky top-0 z-20">
+        <header className="animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20">
             <div className="flex h-8 items-center justify-between w-full">
 
                 <div className="flex items-center gap-2">
